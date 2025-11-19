526f7ec451eef5e585147695d670cf184de0047d
VirgilDucrocq <ducrocq.virgil@gmail.com>
2025-11-19 12:07:48 +0100

    Update ThreadClientServeur.java

--- ThreadClientServeur.java
+++ ThreadClientServeur.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-
+import java.util.*;
 
 public class ThreadClientServeur implements Runnable {
 
@@ -17,18 +17,40 @@ public class ThreadClientServeur implements Runnable {
         this.in = new ObjectInputStream(socket.getInputStream());
     }
 
+    public void envoyerMiseAJourActions(List<Action> actions) throws IOException {
+    
+        this.out.writeObject("UPDATE_ACTIONS"); // Optionnel: envoi d'un marqueur
+        this.out.writeObject(actions);
+        this.out.flush();
+    }
+
     public void run(){
         try {
-            while (true){
-                Transaction t = (Transaction) in.readObject();
-                System.out.println("[Serveur] Reçu: " + t);
-                serveur.traiterTransaction(t);
-                out.writeObject(t);
-                System.out.println("[Serveur] renvoyé: " + t +" "+ (t.estAcceptee()? "ACCEPTEE":"REFUSEE"));
-                out.flush();
+            while (true) {
+                Object obj = in.readObject();
+
+                if (obj instanceof String cmd && cmd.equals("GET_ACTIONS")) {
+                    out.reset();
+                    // Envoi de la liste des actions actuelles au client
+                    out.writeObject(new HashMap<>(serveur.getStockGlobal())); 
+                    out.flush();
+                    continue;
+                }
+
+                if (obj instanceof Transaction t) {
+                    System.out.println("[Serveur] Reçu: " + t);
+                    out.reset();
+                    serveur.traiterTransaction(t);
+                    out.writeObject(t);
+                    out.flush();
+                    System.out.println("[Serveur] Renvoi: " + t + " " + (t.estAcceptee()? "ACCEPTEE":"REFUSEE"));
+                }
+
+                
             }
-        } catch (IOException | ClassNotFoundException e){
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println("Client déconnecté : " + e.getMessage());
-        } 
+        }
     }
+    
 }
