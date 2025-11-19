e4a16891e7ee6da392fa0a54b8d8a86491e4962e
VirgilDucrocq <ducrocq.virgil@gmail.com>
2025-11-19 12:06:49 +0100

    Update Serveur.java

--- Serveur.java
+++ Serveur.java
@@ -14,6 +14,11 @@ public class Serveur{
         this.stockGlobal = new HashMap<>(stockInitial);
     }
 
+    public synchronized List<Action> getActions() {
+    // renvoyer une nouvelle liste pour éviter les modifications externes
+        return new ArrayList<>(stockGlobal.keySet());
+    }
+
     public void demarrer(){
         try {
             serverSocket = new ServerSocket(port);
@@ -32,6 +37,7 @@ public class Serveur{
     }
 
     
+    
     public synchronized void traiterTransaction(Transaction t){
         Action action = t.getAction();
         int qte = t.getQuantite();
@@ -49,7 +55,35 @@ public class Serveur{
         }
     }
 
-    public int getStock(Action a){
-        return stockGlobal.getOrDefault(a, 0);
+    public synchronized Map<Action, Integer> getStockGlobal() {
+        return this.stockGlobal;
     }
+
+    public int getPort(){
+        return this.port;
+    }
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // Création des actions
+        Action apple = new Action("Apple", 100);
+        Action google = new Action("Google", 200);
+        List<Action> actions = Arrays.asList(apple, google);
+
+        // Stock initial côté serveur
+        Map<Action, Integer> stockInitial = new HashMap<>();
+        stockInitial.put(apple, 20);
+        stockInitial.put(google, 20);
+
+        // Création et démarrage du serveur
+        Serveur serveur = new Serveur(5001, stockInitial);
+        new Thread(() -> {serveur.demarrer();}).start();
+        Thread.sleep(500); // laisser le serveur démarrer
+ 
+
+        // Lancement du thread de mise à jour des prix (toutes les 20 sec)
+        MajCoursThread majCours = new MajCoursThread(actions, stockInitial, 2000);
+        new Thread(majCours).start();
+    }
+
+
 }
