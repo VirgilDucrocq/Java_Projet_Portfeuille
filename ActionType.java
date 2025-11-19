e9ea523b2000e20d8d96eaed182a44fe73a77f9e
thomasdem76 <thomdrums76@gmail.com>
2025-11-19 16:32:29 +0100

    Add files via upload

---
+++ ActionType.java
@@ -0,0 +1,25 @@
+public enum ActionType {
+    INDICE_STABLE(0.08, 0.20),
+    VALEUR_TECH_CLASSIQUE(0.10, 0.30),
+    ACTION_VOLATILE_CRYPTO(0.15, 0.60),
+    OBLIGATION_ETAT(0.03, 0.05);
+
+    private final double mu;
+    private final double sigma;
+
+    ActionType(double mu, double sigma) {
+        this.mu = mu;
+        this.sigma = sigma;
+    }
+
+    // Méthodes d'accès (getters)
+    public double getMu() {
+        return mu;
+    }
+
+    public double getSigma() {
+        return sigma;
+    }
+
+}
+
