af84ed6e5d506e49c73e0284e585c9e5451b16ae
VirgilDucrocq <ducrocq.virgil@gmail.com>
2025-11-27 00:50:42 +0100

    rename variables

--- Action.java
+++ Action.java
@@ -7,25 +7,25 @@ import java.util.Objects;
 //###############
 
 //Serializable car on va avoir besoin de les transférer via réseau (actions inlues dans les transactions)
-public class Action implements Serializable {
+public class Action implements Serializable{
     
     //Protection contre la corruption à la deserialisation
     private static final long serialVersionUID = 1L;
-    private String name;
+    private String nom;
     private double prix;
     private ArrayList<Double> historiqueValeurs;
-    private final ActionType type; // Differents type d'actions + ou - stables 
+    private final ActionType typeAction; // Differents type d'actions + ou - stables 
     
     //Constructeur (pas de constructeur par défaut sans paramètres, on veut obligatoirement nom, prix et type)
-    public Action(String name, double prix, ActionType type){
-        if (prix <= 0) {
+    public Action(String nom, double prix, ActionType typeAction){
+        if (prix <= 0){
             throw new IllegalArgumentException("Le prix initial doit être supérieur à 0");
         }
-        this.name = name;
+        this.nom = nom;
         this.prix = prix;
         this.historiqueValeurs = new ArrayList<>();
         this.historiqueValeurs.add(prix);
-        this.type = type;
+        this.typeAction = typeAction;
     }
 
     //Getters
@@ -33,8 +33,8 @@ public class Action implements Serializable {
         return this.historiqueValeurs.size();
     }
 
-    public String getName(){
-        return this.name;
+    public String getNom(){
+        return this.nom;
     }
 
     public double getPrix(){
@@ -45,21 +45,21 @@ public class Action implements Serializable {
         return this.historiqueValeurs;
     }
 
-    public double getMu() {
-        return type.getMu();
+    public double getMu(){
+        return typeAction.getMu();
     }
 
-    public double getSigma() {
-        return type.getSigma();
+    public double getSigma(){
+        return typeAction.getSigma();
     }
     
-    public ActionType getType() {
-        return type;
+    public ActionType getTypeAction(){
+        return typeAction;
     }
 
     //Setter de prix pour pouvoir actualiser
     public void setPrix(double prix){
-        if (prix <= 0) {
+        if (prix <= 0){
             throw new IllegalArgumentException("Le prix initial doit être supérieur à 0");
         }
         this.prix = prix;
@@ -68,24 +68,21 @@ public class Action implements Serializable {
     }
 
     //ToString pour l'affichage
-    public String toString() {
-        return ("Action: " + name + "\nPrix: " + prix);
+    public String toString(){
+        return ("Action: " + nom + "\nPrix: " + prix);
     }
 
 
     // Redefinition de Equals et Hashcode par nom seulement
     // On veut que les actions soient considérées comme les mêmes du moment que le nom est le même
-    public boolean equals(Object o) {
+    public boolean equals(Object o){
         if (this == o) return true;
         if (!(o instanceof Action)) return false;
         Action action = (Action) o;
-        return Objects.equals(name, action.name); // juste le nom
+        return Objects.equals(nom, action.nom); // juste le nom
     }
 
-    public int hashCode() {
-        return Objects.hash(name);
+    public int hashCode(){
+        return Objects.hash(nom);
     }
-
-
-
 }
