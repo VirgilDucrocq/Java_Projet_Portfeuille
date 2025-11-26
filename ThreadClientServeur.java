f44a8955de80d3334bf2c9fe56e7ed9530ad2fbe
VirgilDucrocq <ducrocq.virgil@gmail.com>
2025-11-26 14:15:00 +0100

    Coms + Suppression méthode inutile

--- ThreadClientServeur.java
+++ ThreadClientServeur.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+//gestion de l'écoute (demande d'achat/vente) et de l'envoie des prix, mis à jour par le serveur, avec le client
 public class ThreadClientServeur implements Runnable {
 
     private Socket socket;
@@ -9,6 +10,7 @@ public class ThreadClientServeur implements Runnable {
     private ObjectInputStream in;
     private ObjectOutputStream out;
 
+    //Constructeur
     public ThreadClientServeur(Socket socket, Serveur serveur) throws IOException{
         this.socket = socket;
         this.serveur = serveur;
@@ -17,33 +19,29 @@ public class ThreadClientServeur implements Runnable {
         this.in = new ObjectInputStream(socket.getInputStream());
     }
 
-    public void envoyerMiseAJourActions(List<Action> actions) throws IOException {
-    
-        this.out.writeObject("UPDATE_ACTIONS"); // Optionnel: envoi d'un marqueur
-        this.out.writeObject(actions);
-        this.out.flush();
-    }
 
+    
     public void run(){
         try {
             while (true) {
                 Object obj = in.readObject();
 
+                //Si on nous demande les actions, on envoie le stock du serveur (nouvelle instance pour être sûr de ne rien modifier, sécurité)
                 if (obj instanceof String cmd && cmd.equals("GET_ACTIONS")) {
                     out.reset();
-                    // Envoi de la liste des actions actuelles au client
                     out.writeObject(new HashMap<>(serveur.getStockGlobal())); 
                     out.flush();
                     continue;
                 }
 
                 if (obj instanceof Transaction t) {
-                    System.out.println("[Serveur] Reçu: " + t);
+                    //Balises de contrôle depuis le terminal
+                    //System.out.println("[Serveur] Reçu: " + t);
                     out.reset();
                     serveur.traiterTransaction(t);
                     out.writeObject(t);
                     out.flush();
-                    System.out.println("[Serveur] Renvoi: " + t + " " + (t.estAcceptee()? "ACCEPTEE":"REFUSEE"));
+                    //System.out.println("[Serveur] Renvoi: " + t + " " + (t.estAcceptee()? "ACCEPTEE":"REFUSEE"));
                 }
 
                 
